5022f7e1c18a2d89d2ee64e364e3651a5d112033
Gregory Strydom <gregory.strydom079@gmail.com>
2021-05-24 09:09:13 +0200

    FixingAuthGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,18 +5,21 @@ import { SocialmediaComponent } from './socialmedia/socialmedia.component';
 import { GeneralComponent } from './general/general.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 import { NewPasswordComponent } from './new-password/new-password.component';
 import { EditPasswordComponent } from './edit-password/edit-password.component';
 import { EditPasswordResolver } from './edit-password/edit-password.resolver';
+import { AuthguardService } from './auth/authguard.service';
 
 export const rootRouterConfig: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, children: [] },
   { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
   { path: 'gaming', component: GamingComponent },
   { path: 'social', component: SocialmediaComponent },
   { path: 'general', component: GeneralComponent },
   { path: 'new-user', component: NewPasswordComponent },
-  { path: 'details/:id', component: EditPasswordComponent, resolve:{ data : EditPasswordResolver} }
+  { path: 'details/:id', component: EditPasswordComponent, resolve: { data : EditPasswordResolver} }
 ];
