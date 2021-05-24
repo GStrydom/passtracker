fd99ee2b99353761736b37ac941a5133db2208a0
Gregory Strydom <gregory.strydom079@gmail.com>
2021-05-24 20:39:42 +0200

    Removed unused functions and properties

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,14 +11,12 @@ export class HomeComponent implements OnInit {
 
   searchValue: string = "";
   items: Array<any>;
-  age_filtered_items: Array<any>;
   name_filtered_items: Array<any>;
   totalPasswords: number = 0;
+  duplicates: number = 0;
+  weakpasswords: number = 0;
 
-  constructor(
-    public firebaseService: FirebaseService,
-    private router: Router
-  ) { }
+  constructor(public firebaseService: FirebaseService, private router: Router) {}
 
   ngOnInit() {
     this.getData();
@@ -28,11 +26,19 @@ export class HomeComponent implements OnInit {
     this.firebaseService.getPasswords()
     .subscribe(result => {
       this.items = result;
-      this.age_filtered_items = result;
       this.name_filtered_items = result;
-
       this.totalPasswords = this.name_filtered_items.length;
     })
+  }
+
+  hasDuplicates(arr) {
+    return new Set(arr).size !== arr.length;
+  }
+
+  checkForDuplicates() {
+    if(this.hasDuplicates(this.items)) {
+        this.duplicates += 1;
+    } 
   }
 
   viewDetails(item){
@@ -48,21 +54,7 @@ export class HomeComponent implements OnInit {
     this.firebaseService.searchUsers(value)
     .subscribe(result => {
       this.name_filtered_items = result;
-      this.items = this.combineLists(result, this.age_filtered_items);
+      this.items = result;
     })
   }
-
-  combineLists(a, b){
-    let result = [];
-
-    a.filter(x => {
-      return b.filter(x2 =>{
-        if(x2.payload.doc.id == x.payload.doc.id){
-          result.push(x2);
-        }
-      });
-    });
-    return result;
-  }
-
 }
