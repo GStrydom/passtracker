a9f723071a119ea4ab8896498d7525c4214e9fab
Gregory Strydom <gregory.strydom079@gmail.com>
2021-06-02 08:50:39 +0200

    Removed unneeded logout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,7 +15,6 @@ import { AuthguardService } from './services/auth/authguard.service';
 export const rootRouterConfig: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'logout', component: LogoutComponent },
   { path: 'email', component: EmailComponent, canActivate: [AuthguardService] },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthguardService] },
