ed48619162af0c6ed60e098aba6ea558981df099
Gregory Strydom <gregory.strydom079@gmail.com>
2021-06-07 19:59:42 +0200

    Restructure to include Category Template

--- src/app/components/email/email.component.ts
+++ src/app/components/email/email.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth/auth.service';
-import { Router, Params } from '@angular/router';
+import { Component } from '@angular/core';
+import { CategorytemplateComponent } from '../categorytemplate/categorytemplate.component';
 import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
@@ -17,16 +16,7 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
   ]
 })
 
-export class EmailComponent implements OnInit {
-  searchValue: string = "";
-  items: Array<any>;
-  name_filtered_items: Array<any>;
-  totalPasswords: number = 0;
-  duplicates: number = 0;
-  weakpasswords: number = 0;
-
-  constructor(public firebaseService: AuthService, private router: Router) {}
-
+export class EmailComponent extends CategorytemplateComponent {
   ngOnInit() {
     this.getData();
   }
@@ -37,33 +27,7 @@ export class EmailComponent implements OnInit {
       this.items = result;
       this.name_filtered_items = result;
       this.totalPasswords = this.name_filtered_items.length;
-    })
-  }
-
-  hasDuplicates(arr) {
-    return new Set(arr).size !== arr.length;
-  }
-
-  checkForDuplicates() {
-    if(this.hasDuplicates(this.items)) {
-        this.duplicates += 1;
-    } 
-  }
-
-  viewDetails(item){
-    this.router.navigate(['/details/'+ item.payload.doc.id]);
-  }
-
-  capitalizeFirstLetter(value){
-    return value.charAt(0).toUpperCase() + value.slice(1);
-  }
-
-  searchByName(){
-    let value = this.searchValue.toLowerCase();
-    this.firebaseService.searchUsers(value)
-    .subscribe(result => {
-      this.name_filtered_items = result;
-      this.items = result;
+      this.checkDuplicates(result);
     })
   }
 }
--- src/app/components/general/general.component.ts
+++ src/app/components/general/general.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth/auth.service';
-import { Router, Params } from '@angular/router';
+import { Component } from '@angular/core';
+import { CategorytemplateComponent } from '../categorytemplate/categorytemplate.component';
 import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
@@ -16,19 +15,8 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
     ])
   ]
 })
-export class GeneralComponent implements OnInit {
 
-  searchValue: string = "";
-  items: Array<any>;
-  age_filtered_items: Array<any>;
-  name_filtered_items: Array<any>;
-  totalPasswords: number = 0;
-
-  constructor(
-    public firebaseService: AuthService,
-    private router: Router
-  ) { }
-
+export class GeneralComponent extends CategorytemplateComponent {
   ngOnInit() {
     this.getData();
   }
@@ -37,40 +25,9 @@ export class GeneralComponent implements OnInit {
     this.firebaseService.getGeneralPasswords()
     .subscribe(result => {
       this.items = result;
-      this.age_filtered_items = result;
       this.name_filtered_items = result;
       this.totalPasswords = this.name_filtered_items.length;
+      this.checkDuplicates(result);
     })
   }
-
-  viewDetails(item){
-    this.router.navigate(['/details/'+ item.payload.doc.id]);
-  }
-
-  capitalizeFirstLetter(value){
-    return value.charAt(0).toUpperCase() + value.slice(1);
-  }
-
-  searchByName(){
-    let value = this.searchValue.toLowerCase();
-    this.firebaseService.searchUsers(value)
-    .subscribe(result => {
-      this.name_filtered_items = result;
-      this.items = this.combineLists(result, this.age_filtered_items);
-    })
-  }
-
-  combineLists(a, b){
-    let result = [];
-
-    a.filter(x => {
-      return b.filter(x2 =>{
-        if(x2.payload.doc.id == x.payload.doc.id){
-          result.push(x2);
-        }
-      });
-    });
-    return result;
-  }
-
 }
