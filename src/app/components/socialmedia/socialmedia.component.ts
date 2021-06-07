9e09d4faa22eb74d5075918e52dc4c9a48b208d8
Gregory Strydom <gregory.strydom079@gmail.com>
2021-06-07 19:59:59 +0200

    Restructure

--- src/app/components/socialmedia/socialmedia.component.ts
+++ src/app/components/socialmedia/socialmedia.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth/auth.service';
-import { Router, Params } from '@angular/router';
+import { Component } from '@angular/core';
+import { CategorytemplateComponent } from '../categorytemplate/categorytemplate.component';
 import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
-  selector: 'app-gaming',
+  selector: 'app-socialmedia',
   templateUrl: './socialmedia.component.html',
   styleUrls: ['./socialmedia.component.scss'],
   animations: [
@@ -16,19 +15,8 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
     ])
   ]
 })
-export class SocialmediaComponent implements OnInit {
 
-  searchValue: string = "";
-  items: Array<any>;
-  age_filtered_items: Array<any>;
-  name_filtered_items: Array<any>;
-  totalPasswords: number = 0;
-
-  constructor(
-    public firebaseService: AuthService,
-    private router: Router
-  ) { }
-
+export class SocialmediaComponent extends CategorytemplateComponent {
   ngOnInit() {
     this.getData();
   }
@@ -37,41 +25,9 @@ export class SocialmediaComponent implements OnInit {
     this.firebaseService.getSocialPasswords()
     .subscribe(result => {
       this.items = result;
-      this.age_filtered_items = result;
       this.name_filtered_items = result;
-
       this.totalPasswords = this.name_filtered_items.length;
+      this.checkDuplicates(result);
     })
   }
-
-  viewDetails(item){
-    this.router.navigate(['/details/'+ item.payload.doc.id]);
-  }
-
-  capitalizeFirstLetter(value){
-    return value.charAt(0).toUpperCase() + value.slice(1);
-  }
-
-  searchByName(){
-    let value = this.searchValue.toLowerCase();
-    this.firebaseService.searchUsers(value)
-    .subscribe(result => {
-      this.name_filtered_items = result;
-      this.items = this.combineLists(result, this.age_filtered_items);
-    })
-  }
-
-  combineLists(a, b){
-    let result = [];
-
-    a.filter(x => {
-      return b.filter(x2 =>{
-        if(x2.payload.doc.id == x.payload.doc.id){
-          result.push(x2);
-        }
-      });
-    });
-    return result;
-  }
-
 }
